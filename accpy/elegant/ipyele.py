248e7edf118ef443bbd2f2998fa9ccf212b339bd
Felix Kramer <felix.kramer@physik.hu-berlin.de>
2017-10-10 07:00:36 +0200

    elegant

--- accpy/elegant/ipyele.py
+++ accpy/elegant/ipyele.py
@@ -4,13 +4,14 @@ author:     felix.kramer(at)physik.hu-berlin.de
 """
 from __future__ import print_function, division
 from subprocess import Popen, PIPE, STDOUT
-from numpy import (shape, array, max as npmax, argmax, roll, float64, core, 
-                   min, linspace)
+from numpy import (shape, array, max as npmax, argmax, roll, float64, core,
+                   min as npmin, linspace, where)
 from matplotlib.pylab import (plot, subplot, xlabel, ylabel, twinx, gca, xlim,
                               ylim, annotate, tight_layout)
-import matplotlib.patches as patches
+from matplotlib.patches import Polygon, Rectangle
 from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, VPacker
 from matplotlib import cm
+from pylab import figure
 from IPython.display import display, Javascript
 from . import sdds
 from ..simulate import const
@@ -160,59 +161,115 @@ def trackplot2(datadict, particles=8, abscissa='t'):
     eleplot(datadict, 'y', 'yp', '.', sel=range(particles))
 
 
-def twissplot(data):
-    L = npmax(data['s'])
+def drawlatt(ax, data):
+    s = data['s']
+    ax.set_xlim(npmin(s), npmax(s))
+
+    yl = ax.get_ylim()[1]
+    dy = yl*0.1
+    yc = yl+dy/2
+
+    i = 0
+    while i < len(s):
+        et = data['ElementType'][i]
+        try:
+            li = where(data['ElementType'][i:] != et)[0][0]
+        except:
+            li = len(data['ElementType'][i:])
+
+        if i == 0:
+            si = s[i]
+        else:
+            si = s[i - 1]
+        sf = s[i + li - 1]
+        dx = sf - si
+        i += li
+
+        if et == 'DRIF':
+            ax.add_patch(Polygon(xy=array([[si, yc], [sf, yc]]), closed=False, color='w', clip_on=False, lw=2, zorder=110))
+            continue
+        elif et == 'CSBEND':
+            c = 'yellow'
+        elif et == 'KQUAD':
+            c = 'red'
+        elif et == 'KSEXT':
+            c = 'green'
+        else:
+            c = 'none'
+        if c != 'none':
+            ax.add_patch(Rectangle(xy=(si, yl), width=dx, height=dy, ec=None, facecolor=c, clip_on=False, zorder=111))
+
+
+def multicolorylab(ax, lablist, collist):
+    boxs = [TextArea(l, textprops=dict(color=c, rotation=90, ha='center', va='bottom')) for l, c in zip(lablist, collist)]
+    ybox = VPacker(children=boxs[::-1], pad=0, sep=5)
+    anchored_ybox = AnchoredOffsetbox(loc=8, child=ybox, pad=0., frameon=False,
+                                      bbox_to_anchor=(-.045, 0.41),
+                                      bbox_transform=ax.transAxes, borderpad=0.)
+    ax.add_artist(anchored_ybox)
+
+
+def drawtwiss(data, fs):
+    fig = figure(figsize=fs)
+    ax1 = fig.add_subplot(111)
+    ax2 = twinx()
+    ax1.plot(data['s'], data['betax'], '-g')
+    ax1.plot(data['s'], data['betay'], '-b')
+    ax1.set_xlabel('s')
+    ax1 = gca()
+    multicolorylab(ax1, ['$\\beta_x$', ', ','$\\beta_y$', ' / (m)'], ['g', None, 'b', None])
+    ax2.plot(data['s'], data['etax'], '-r')
+    ax2.set_ylabel(r'$\eta_x$ / (m)', color='r')
+    drawlatt(ax1, data)
+    ax2.grid(None)
+    return fig
+
+
+def twissdata(data):
     print('General:')
-    print('    L = {:}'.format(L))
+    print('    L = {:}'.format(npmax(data['s'])))
     print('    E = {:}'.format(data['pCentral'][0]))
     print('    Eloss = {:}'.format(data['U0'][0]))
-    
     print('\nTunes:')
     print('    Qx = {:.6}'.format(data['nux'][0]))
     print('    Qy = {:.6}'.format(data['nuy'][0]))
-    
     print('\nChromaticities:')
     print('    ' + uc.greek.xi + 'x = {:.6}'.format(data['dnux/dp'][0]))
     print('    ' + uc.greek.xi + 'y = {:.6}'.format(data['dnuy/dp'][0]))
-    
     print('\nMomentum Compaction Factor:')
     print('    ' + uc.greek.alpha + 'p = {:.5e}'.format(data['alphac'][0]))
-    
     print('\nRadiation Damping times:')
     print('    ' + uc.greek.tau + 'x = {:.6} ms'.format(1e3*data['taux'][0]))
     print('    ' + uc.greek.tau + 'y = {:.6} ms'.format(1e3*data['tauy'][0]))
     print('    ' + uc.greek.tau + uc.greek.delta + ' = {:.6} ms'.format(1e3*data['taudelta'][0]))
-    
     print('\nDamping partition factors:')
     print('    Jx = {:.6}'.format(data['Jx'][0]))
     print('    Jy = {:.6}'.format(data['Jy'][0]))
     print('    J' + uc.greek.delta + ' = {:.6}'.format(data['Jdelta'][0]))
-    
     print('\nHorizontal equilibrium geometric and normalized emittances:')
     print('    ' + uc.greek.epsilon + 'x = {:.6}'.format(data['ex0'][0]))
     print('    ' + uc.greek.epsilon + 'x* = {:.6}'.format(data['enx0'][0]))
-    
     print('\nEquilibrium fractional rms energy spread:')
     print('    ' + uc.greek.epsilon + uc.greek.delta + ' = {:.6e}'.format(data['Sdelta0'][0]))
-    
     print('\nHigher Order::')
     print('    ' + uc.greek.xi + 'x2 = {:}'.format(data['dnux/dp2'][0]))
     print('    ' + uc.greek.xi + 'y2 = {:}'.format(data['dnuy/dp2'][0]))
     print('    ' + uc.greek.xi + 'x3 = {:}'.format(data['dnux/dp3'][0]))
     print('    ' + uc.greek.xi + 'y3 = {:}'.format(data['dnuy/dp3'][0]))
     print('    ' + uc.greek.alpha + 'p2 = {:e}'.format(data['alphac2'][0]))
+    return
 
-    eleplot(data, 's', 'betax', '-g', label=False)
-    eleplot(data, 's', 'betay', '-b', label=False)
-    twinx()
-    eleplot(data, 's', 'etax', '-r', label=False)
 
-    # Latteice graphics vertical position and size (axis coordinates!)
-    lypos = gca().get_ylim()[1]
-    tp = Twissplot(lypos = lypos, lysize = lypos*0.12)
-    tp.axislabels(yscale=0.5)
-    tp.paintlattice(data, 0, L, ec=False, fscale=2)
-    xlim(0, L)
+def twissplot(data, zoom=False, fs=[16, 9]):
+    if zoom:
+        starti, endi = argmax(data['s'] >= zoom[0]), argmax(data['s'] >= zoom[1])
+        clipdata = {}
+        for clip in ['s', 'betax', 'betay', 'etax', 'ElementType']:
+            clipdata[clip] = data[clip][starti:endi]
+        fig = drawtwiss(clipdata, fs)
+        return fig
+    fig = drawtwiss(data, fs)
+    return fig
 
 
 def b2twissplot(coldict, pardict):
@@ -328,7 +385,7 @@ class Twissplot():
                 ecol='none'
 
             if col != 'none':
-                gca().add_patch(patches.Rectangle((start, ycenter-0.5*ysize), l,ysize, ec=ecol, facecolor=col,clip_on=False, zorder = 101))
+                gca().add_patch(Rectangle((start, ycenter-0.5*ysize), l,ysize, ec=ecol, facecolor=col,clip_on=False, zorder = 101))
                 if labels:
                     fs = 80 / (s1-s0) * fscale
                     if et[i] == 'KSEXT':
@@ -414,7 +471,7 @@ def trackplot(datadict, turns=False):
                 else:
                     [plot(datadict[x][:, part], datadict[y][:, part], '.', color=col) for part, col in enumerate(colors)]
                     xlabel(x)
-            ylabel(y)   
+            ylabel(y)
             tight_layout()
     return
 
@@ -437,14 +494,14 @@ def showbun(datadict):
 
 
 def mybunch(bunchname, ranges, E_mev):
-    
+
     cl = const.cl
     qe = const.qe
     me = const.me
     E0 = me*cl**2/qe  # eV
     gamma = 1 + E_mev*1e6/E0
     print('gamma = {}'.format(gamma))
-    
+
     N = len(ranges['x'])
     bunch = sdds.SDDS(0)  # what does the index mean?
     bunch.setDescription('my predefined bunch', 'bunched-beam phase space')
