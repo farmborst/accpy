d19df9ac4ec24333d079a043e71bb364aabf52b9
Felix Kramer <felix.kramer@physik.hu-berlin.de>
2018-02-12 11:23:31 +0100

    gauss2d

--- accpy/math/specialfuns.py
+++ accpy/math/specialfuns.py
@@ -2,13 +2,14 @@
 ''' accpy.math.specialfuns
 author:     felix.kramer(at)physik.hu-berlin.de
 '''
-from numpy import exp, sqrt, pi, cos, linspace, logspace, log2
+from numpy import exp, sqrt, pi, sin, cos, linspace, logspace, log2
 
 
-def gauss(x, a, mu, sigma, offset):
-    ''' gauss(x, a, mu, sigma, offset) = a*exp(-(((x-mu)/sigma)**2)/2)/sigma/sqrt(2*pi)+offset
+def gauss(x, amp, mu, sigma, offset):
+    ''' gauss(x, a, mu, sigma, offset) =
+        a*exp(-(((x-mu)/sigma)**2)/2)/sigma/sqrt(2*pi)+offset
     x      - input variable
-    a      - (parameter)
+    amp    - amplitude (parameter)
     mu     - expected value (parameter)
     sigma  - standard deviation (parameter)
     offset - (parameter)
@@ -19,7 +20,34 @@ def gauss(x, a, mu, sigma, offset):
          full width at tenth of maximum
          FWTM = 2*sqrt(2*ln(10))*sigma
     '''
-    return a*exp(-(((x-mu)/sigma)**2)/2)/sigma/sqrt(2*pi)+offset
+    return amp * exp( -(((x-mu)/sigma)**2)/2 ) / sigma/sqrt(2*pi) + offset
+
+
+def gauss2D((x, y), amp, (mux, muy), (sigx, sigy), offset, theta=0):
+    ''' gauss2D(x, a, mu, sigma, offset) =
+        amp*exp(-(((x-x0)/sigma)**2)/2)/sigma/sqrt(2*pi) + offset
+    x, y       - input variables
+    amp        - amplitude (parameter)
+    mux, muy   - expected value (parameter)
+    sigx, sigy - standard deviation (parameter)
+    theta      - rotation (parameter)
+    offset     - (parameter)
+
+    infos:
+         full width at half maximum
+         FWHM = 2*sqrt(2*ln(2))*sigma
+         full width at tenth of maximum
+         FWTM = 2*sqrt(2*ln(10))*sigma
+    '''
+    if theta != 0:
+        sigx2, sigy2 = sigx**2, sigy**2
+        sinth2, costh2 = sin(theta)**2, cos(theta)**2
+        a = (costh2/sigx2 + sinth2/sigy2)/2
+        b = sin(2*theta)/4 * (1/sigy2 - 1/sigx2)
+        c = (sinth2/sigx2 + costh2/sigy2)/2
+        return amp * exp( -a*(x-mux)**2 - 2*b*(x-mux)*(y-muy) - c*(y-muy)**2 ) /sigx/sigy/(2*pi) + offset
+    else:
+        return amp * exp( -(((x-mux)/sigx)**2 + ((y-muy)/sigy)**2)/2 ) /sigx/sigy/(2*pi) + offset
 
 
 def hanning(N):
