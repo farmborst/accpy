2af2768efb033c1c40696dacff9202ef80322ca4
inp@hfxdesktop <felix.armborst@physik.hu-berlin.de>
2019-10-30 00:23:22 +0100

    jupyter dataio

--- accpy/elegant/ipyele.py
+++ accpy/elegant/ipyele.py
@@ -210,7 +210,7 @@ def drawlatt(ax, data, size=0.1):
         i += li
         
         # get dict of nagnets and strengths
-        latticename = str(data['description'][0]).split('lattice: ')[1]
+        latticename = bytes(data['description'][0]).decode('utf-8').split('lattice: ')[1]
         magdict = {}
         with open(latticename, 'r') as fh:
             for line in fh:
